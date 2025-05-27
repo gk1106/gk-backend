7ad363ad73c81594f7c3bbdee23ef6bf8282d081
gk1106 <gk753778@gmail.com>
2025-05-27 23:11:37 +0530

    bug fix in env files

--- src/main/java/com/gk/website/portfolio/PortfolioApplication.java
+++ src/main/java/com/gk/website/portfolio/PortfolioApplication.java
@@ -3,22 +3,18 @@ package com.gk.website.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
-        System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+        // These are optional unless you're explicitly using System.getProperty()
+        System.setProperty("DB_URL", System.getenv("DB_URL"));
+        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+        System.setProperty("EMAIL_USERNAME", System.getenv("EMAIL_USERNAME"));
+        System.setProperty("EMAIL_PASSWORD", System.getenv("EMAIL_PASSWORD"));
 
         SpringApplication.run(PortfolioApplication.class, args);
-	}
-
+    }
 }
