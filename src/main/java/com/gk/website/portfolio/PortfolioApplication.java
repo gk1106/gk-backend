96421559b5cd84ddeda0ecba46a7ae4bcbdae8ac
gk1106 <gk753778@gmail.com>
2025-05-27 23:20:26 +0530

    final bug fix

--- src/main/java/com/gk/website/portfolio/PortfolioApplication.java
+++ src/main/java/com/gk/website/portfolio/PortfolioApplication.java
@@ -8,13 +8,6 @@ public class PortfolioApplication {
 
     public static void main(String[] args) {
 
-        // These are optional unless you're explicitly using System.getProperty()
-        System.setProperty("DB_URL", System.getenv("DB_URL"));
-        System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
-        System.setProperty("EMAIL_USERNAME", System.getenv("EMAIL_USERNAME"));
-        System.setProperty("EMAIL_PASSWORD", System.getenv("EMAIL_PASSWORD"));
-
         SpringApplication.run(PortfolioApplication.class, args);
     }
 }
